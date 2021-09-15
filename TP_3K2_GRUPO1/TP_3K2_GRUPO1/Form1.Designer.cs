26366fd2c371f3bd548b5e7b9df20395563def85
YagoGandara <yago@gandara.com.ar>
2021-09-15 19:11:41 -0300

    Login
    Hice la ventana de login basica, hay que modificarla

--- TP_3K2_GRUPO1/TP_3K2_GRUPO1/Form1.Designer.cs
+++ TP_3K2_GRUPO1/TP_3K2_GRUPO1/Form1.Designer.cs
@@ -29,13 +29,81 @@ namespace TP_3K2_GRUPO1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.LblContra = new System.Windows.Forms.Label();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.TxtContra = new System.Windows.Forms.TextBox();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.Location = new System.Drawing.Point(95, 87);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(61, 17);
+            this.LblUsuario.TabIndex = 0;
+            this.LblUsuario.Text = "Usuario:";
+            this.LblUsuario.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblContra
+            // 
+            this.LblContra.AutoSize = true;
+            this.LblContra.Location = new System.Drawing.Point(95, 140);
+            this.LblContra.Name = "LblContra";
+            this.LblContra.Size = new System.Drawing.Size(54, 17);
+            this.LblContra.TabIndex = 1;
+            this.LblContra.Text = "Contra:";
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(183, 82);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(142, 22);
+            this.TxtUsuario.TabIndex = 2;
+            // 
+            // TxtContra
+            // 
+            this.TxtContra.Location = new System.Drawing.Point(183, 135);
+            this.TxtContra.Name = "TxtContra";
+            this.TxtContra.PasswordChar = '*';
+            this.TxtContra.Size = new System.Drawing.Size(142, 22);
+            this.TxtContra.TabIndex = 3;
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.Location = new System.Drawing.Point(250, 194);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.BtnIngresar.TabIndex = 4;
+            this.BtnIngresar.Text = "Ingresar";
+            this.BtnIngresar.UseVisualStyleBackColor = true;
+            this.BtnIngresar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.TxtContra);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.LblContra);
+            this.Controls.Add(this.LblUsuario);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.Label LblContra;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.TextBox TxtContra;
+        private System.Windows.Forms.Button BtnIngresar;
     }
 }
 
